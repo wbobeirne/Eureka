375c72242839810bdedc223acc940c3fee3fb128
Will O'Beirne <will@okcupid.com>
2014-09-29 22:12:54 -0400

    Reworked how menu is handled.

--- assets/eureka.js
+++ assets/eureka.js
@@ -1,12 +1,74 @@
 (function() {
-  var $;
+  var $,
+    _this = this;
 
   $ = jQuery;
 
   window.Eureka = {
     init: function() {
-      $('body').addClass('eureka-loaded');
-      return this.handleFeatureBoard();
+      this.addNavClasses();
+      this.buildMoreNav();
+      this.handleFeatureBoard();
+      return $('body').addClass('eureka-loaded');
+    },
+    addNavClasses: function() {
+      return $('.top_nav ul li').each(function(idx, el) {
+        var $el, id;
+        $el = $(el);
+        if ($el.find('.nav-title').length) {
+          id = $el.find('.nav-title').html();
+          if (id) {
+            $el.addClass('primary');
+          }
+        } else {
+          id = $el.find('a').html();
+          if (id) {
+            $el.addClass('secondary');
+          }
+        }
+        if (id) {
+          return $el.attr('id', 'nav-' + id.trim().toLowerCase().replace(' ', '-'));
+        }
+      });
+    },
+    buildMoreNav: function() {
+      var $moreItems, $navItems, navItems, primary, secondary, _i, _j, _len, _len1, _ref;
+      navItems = [];
+      $('.top_nav li .nav-title').each(function(idx, el) {
+        var $navItem, link, secondaryItems, title;
+        $navItem = $(el).closest('li');
+        title = el.innerHTML;
+        link = $navItem.find('a').first().attr('href');
+        secondaryItems = [];
+        $navItem.find('li').each(function(idx, el) {
+          var $subnavItem;
+          $subnavItem = $(el);
+          return secondaryItems.push({
+            title: $subnavItem.find('a').html(),
+            link: $subnavItem.find('a').attr('href')
+          });
+        });
+        return navItems.push({
+          title: title,
+          link: link,
+          subnavs: secondaryItems
+        });
+      });
+      $moreItems = $("<li id=\"nav-more-items\">\n	<a href=\"javascript:void(0)\" class=\"toggle\">\n		<span class=\"nav-title\">More items</span>\n	</a>\n	<div class=\"nav-items\">\n\n	</div>\n</li>");
+      $navItems = $moreItems.find('.nav-items');
+      for (_i = 0, _len = navItems.length; _i < _len; _i++) {
+        primary = navItems[_i];
+        $navItems.append("<div class=\"primary\">\n	<a href=\"" + primary.link + "\">\n		<span>" + primary.title + "</span>\n	</a>\n</div>");
+        _ref = primary.subnavs;
+        for (_j = 0, _len1 = _ref.length; _j < _len1; _j++) {
+          secondary = _ref[_j];
+          $navItems.append("<div class=\"secondary\">\n	<a href=\"" + secondary.link + "\">\n		<span>" + secondary.title + "</span>\n	</a>\n</div>");
+        }
+      }
+      $('ul.nav:not(.right-nav)').append($moreItems);
+      return $moreItems.find('.toggle').off('click').on('click', function() {
+        return $moreItems.toggleClass('expanded');
+      });
     },
     handleFeatureBoard: function() {
       if (!$('.project-board-container').length) {
@@ -17,6 +79,12 @@
     }
   };
 
-  Eureka.init();
+  $(document).ready(function() {
+    return Eureka.init();
+  });
+
+  $(document).on('page:load', function() {
+    return Eureka.init();
+  });
 
 }).call(this);
--- eureka.coffee
+++ eureka.coffee
@@ -3,10 +3,92 @@ $ = jQuery
 window.Eureka = {
 
 	init: ->
+		@addNavClasses()
+		@buildMoreNav()
+		@handleFeatureBoard()
+		
 		$('body').addClass('eureka-loaded')
 
-		@handleFeatureBoard()
+	# Adds classes to the navigation for styling.
+	addNavClasses: ->
+		$('.top_nav ul li').each((idx, el) ->
+			$el = $(el)
+			if $el.find('.nav-title').length
+				id = $el.find('.nav-title').html()
+				if id
+					$el.addClass('primary')
+			else
+				id = $el.find('a').html()
+				if id
+					$el.addClass('secondary')
 
+			
+			if id
+				$el.attr('id', 'nav-' + id.trim().toLowerCase().replace(' ', '-'))
+		)
+
+	# Moves good navigation to top level items, bad navigation to extras menu
+	buildMoreNav: ->
+		# Organize nav data
+		navItems = []
+		$('.top_nav li .nav-title').each((idx, el) ->
+			$navItem = $(el).closest('li')
+			title = el.innerHTML
+			link = $navItem.find('a').first().attr('href')
+			secondaryItems = []
+
+			$navItem.find('li').each((idx, el) ->
+				$subnavItem = $(el)
+				secondaryItems.push({
+					title: $subnavItem.find('a').html()
+					link: $subnavItem.find('a').attr('href')
+				})
+			)
+
+			navItems.push({
+				title: title
+				link: link
+				subnavs: secondaryItems
+			})
+		)
+
+		# Re-add to DOM in extras menu
+		$moreItems = $("""
+			<li id="nav-more-items">
+				<a href="javascript:void(0)" class="toggle">
+					<span class="nav-title">More items</span>
+				</a>
+				<div class="nav-items">
+
+				</div>
+			</li>
+		""")
+		$navItems = $moreItems.find('.nav-items')
+
+		for primary in navItems
+			$navItems.append("""
+				<div class="primary">
+					<a href="#{primary.link}">
+						<span>#{primary.title}</span>
+					</a>
+				</div>
+			""")
+
+			for secondary in primary.subnavs
+				$navItems.append("""
+					<div class="secondary">
+						<a href="#{secondary.link}">
+							<span>#{secondary.title}</span>
+						</a>
+					</div>
+				""")
+
+		$('ul.nav:not(.right-nav)').append($moreItems)
+		
+		# Bind events
+		$moreItems.find('.toggle').off('click').on('click', ->
+			$moreItems.toggleClass('expanded')
+		)
 
 	# Handles the display at products/*/feature_cards
 	handleFeatureBoard: ->
@@ -17,4 +99,11 @@ window.Eureka = {
 }
 
 # Run init, or not, based on extension status
-Eureka.init()
+$(document).ready( =>
+	Eureka.init()
+)
+# For turbolinks
+$(document).on('page:load', =>
+	Eureka.init()
+)
+
